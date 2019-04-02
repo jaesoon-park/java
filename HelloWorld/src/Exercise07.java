56f11a1417c438b939d0ae23fc17e3e7b5a3a8ea
jaesoon-park <wotns2606@cu.ac.kr>
2019-04-02 16:26:18 +0900

    study_java1

---
+++ HelloWorld/src/Exercise07.java
@@ -0,0 +1,19 @@
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		int max = 0;
+		int[] array = {1,5,3,8,2};
+		
+		for(int i = 0; i < array.length; i++) {
+			if(max < array[i]) {
+				max = array[i];
+			}
+		}
+		
+		System.out.println("max: "+max);
+	}
+
+}
